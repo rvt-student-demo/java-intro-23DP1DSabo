2673d1eba10cf696c744a11629941c72db752a12
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-09-03 11:01:11 +0000

    pirmo stundu nobeigums

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,12 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-            String word = "Zirgstīgstrinkšķināminstruments";
-            int number = 1234567890;
-            boolean nezinu = false;
-
-            System.out.println(word);
-
+        // Once upon a time
+        System.out.println("Nothing happened");
 
     }
 
