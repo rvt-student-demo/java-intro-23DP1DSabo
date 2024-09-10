160ef072ccb0c4ca2a91fb80c3bbf9607c776c39
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-09-10 10:31:39 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,13 +1,29 @@
 package lv.rvt;
 
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
-        // Once upon a time
-        System.out.println("Nothing happened");
-        String virkne = "zirgstīgstrinkšķināminstrumnts";
-        System.out.println(virkne);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadiet savu vārdu: ");
+        String vards = scanner.nextLine();
+        System.out.println("");
+
+        System.out.println("Ievadet savu uzvārdu: ");
+        String uzvards = scanner.nextLine();
+        System.out.println("");
+
+        System.out.println("Ievadiet savu grupu: ");
+        String grupa = scanner.nextLine();
+        System.out.println("");
+
+
+        System.out.println("Students: " + vards + " " + uzvards + ", grupa: " + grupa);
+
+
 
     }
 
