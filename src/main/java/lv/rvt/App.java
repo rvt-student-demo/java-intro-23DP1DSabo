761890e14673131d37d0dbe3d8c58427f9637807
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-01 07:48:31 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,12 +5,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        printText();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int numero = Integer.valueOf(scanner.nextLine());
+
+        printText(numero);
     }
 
-    public static void printText() {
-        System.out.println("In a hole in the ground there lived a method");
-        System.out.println("");
+    public static void printText(int numero) {
+        for(int i = 0; i < numero; i++) {
+            System.out.println("In a hole in the ground there lived a method");
+        }
     }
 }
     
