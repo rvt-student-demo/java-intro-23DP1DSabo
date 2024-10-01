edd5132178a9702e5fc7ba39b7d4fec08af9eb77
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-01 07:40:44 +0000

    In a hole in the ground

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,38 +5,12 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        int sum = 0;
-        int n = 0;
-        double avg = 0;
-        int even = 0;
-        int odd = 0;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Give numbers:");
-        while (true) {
-            int numero = Integer.valueOf(scanner.nextLine());
-            if (numero == -1) {
-                break;
-            }
-            sum += numero;
-            n += 1;
-            avg = (double)sum / n;
-            if (numero % 2 == 0) {
-                even += 1;
-            }
-            if (numero % 2 == 1) {
-                odd += 1;
-            }
-        }
-
-        System.out.println("Thx bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + n);
-        System.out.println("Average: " + avg);
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
-
-        }
+        printText();
     }
+
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+        System.out.println("");
+    }
+}
     
