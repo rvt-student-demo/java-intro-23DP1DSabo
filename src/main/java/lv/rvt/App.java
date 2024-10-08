8e0bfc8d1f6783c65aefaa3ced1f9d5cf27f1405
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-08 06:28:57 +0000

    Printing a square

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,16 +5,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        printStars(5); 
-        printStars(3); 
-        printStars(9); 
+        printSquare(4); 
     }
 
-    public static void printStars(int number) {
-        for (int i = 1; i < number + 1; i++) {
-            System.out.print("*");
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
 
