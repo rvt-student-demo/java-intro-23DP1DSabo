39c0c42821a8282643dc3b2f6c65eeb3dcd972ff
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-15 10:37:01 +0000

    Third element (nepabeigts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,21 +1,33 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        printTriangle(4); 
-    }
-
-    public static void printTriangle(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            } 
-            System.out.println();
+        ArrayList<String> names = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            Scanner scanner = new Scanner(System.in);
+            String name = String.valueOf(scanner.nextLine());
+            if (name == "") {
+                break;
+            }
+            names.add(name);
         }
+        
+        System.out.println(names.get(0));
+
     }
+ 
+    
 }
+
+// Pievienojam elementus
+// intList.add(1);
+// Saņemt elementus
+// intList.get(0);
+// Cik ir elmentu masīvā?
+// intList.size();
+
 
     
