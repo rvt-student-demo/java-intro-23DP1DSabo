0f951655e8ad4b862dab5c0921add4944b3eb7ac
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-22 07:44:19 +0000

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,29 +5,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        ArrayList<String> names = new ArrayList<>();
-        for (int i = 0; i < names.size(); i++) {
-            Scanner scanner = new Scanner(System.in);
-            String name = String.valueOf(scanner.nextLine());
-            if (name == "") {
-                break;
-            }
-            names.add(name);
-        }
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
         
-        System.out.println(names.get(0));
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+        
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10); 
 
     }
- 
-    
-}
 
-// Pievienojam elementus
-// intList.add(1);
-// Saņemt elementus
-// intList.get(0);
-// Cik ir elmentu masīvā?
-// intList.size();
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        for (int number: numbers) {
+            if (number >= lowerLimit && number <= upperLimit) {
+                System.out.println(number);
+            }
+        }
+    }
 
-
-    
+} 
