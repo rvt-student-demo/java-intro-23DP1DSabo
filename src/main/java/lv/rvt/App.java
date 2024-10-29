796bc3a74ffd743b43103c63ad4435bbc9eadeb3
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-10-29 12:11:05 +0000

    .

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,28 +5,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(3);
-        numbers.add(2);
-        numbers.add(6);
-        numbers.add(-1);
-        numbers.add(5);
-        numbers.add(1);
-        
-        System.out.println("The numbers in the range [0, 5]");
-        printNumbersInRange(numbers, 0, 5);
-        
-        System.out.println("The numbers in the range [3, 10]");
-        printNumbersInRange(numbers, 3, 10); 
+        Person ada = new Person("Ada");
+        Person antti = new Person("Antti");
+        Person martin = new Person("Martin");
 
-    }
-
-    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
-        for (int number: numbers) {
-            if (number >= lowerLimit && number <= upperLimit) {
-                System.out.println(number);
-            }
-        }
+        ada.printPerson();
+        antti.printPerson();
+        martin.printPerson();
     }
 
 } 
---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,13 @@
+package lv.rvt;
+
+public class Product {
+    public Product (String initialName, double initialPrice, int initialQuantity) {
+        this.name
+
+
+    }
+
+    public void printProduct() {
+        System.out.println();
+    }
+}
