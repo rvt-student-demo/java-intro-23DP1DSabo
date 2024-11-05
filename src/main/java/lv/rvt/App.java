c93199f4d4dcd85fb32e58e5d9c78adb0dc2e0f8
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-11-05 07:41:20 +0000

    Decreasing counter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,25 +3,17 @@ package lv.rvt;
 import java.util.*;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Person ada = new Person("Ada");
-        Person antti = new Person("Antti");
-        Person martin = new Person("Martin");
+    public static void main( String[] args ) {
 
-        ada.printPerson();
-        antti.printPerson();
-        martin.printPerson();
+        DecreasingCounter counter = new DecreasingCounter(10);
 
-        System.out.println("");
+        counter.printValue();
 
-        Product produkts1 = new Product("Banana", 1.1, 13);
-        Product produkts2 = new Product("Apple", 0.6, 12);
-        Product produkts3 = new Product("human", 0.02, 50);
+        counter.decrement();
+        counter.printValue();
 
-        produkts1.printProduct();
-        produkts2.printProduct();
-        produkts3.printProduct();
+        counter.decrement();
+        counter.printValue();
     }
 
 } 
