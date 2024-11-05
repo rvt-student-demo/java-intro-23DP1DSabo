211ba4b85de4a83f6a6c703967e4549cbd1c1fd2
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-11-05 07:21:13 +0000

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,6 +12,16 @@ public class App
         ada.printPerson();
         antti.printPerson();
         martin.printPerson();
+
+        System.out.println("");
+
+        Product produkts1 = new Product("Banana", 1.1, 13);
+        Product produkts2 = new Product("Apple", 0.6, 12);
+        Product produkts3 = new Product("human", 0.02, 50);
+
+        produkts1.printProduct();
+        produkts2.printProduct();
+        produkts3.printProduct();
     }
 
 } 
--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -1,13 +1,19 @@
 package lv.rvt;
 
 public class Product {
-    public Product (String initialName, double initialPrice, int initialQuantity) {
-        this.name
 
+    private String Name;
+    private double Price;
+    private int Quantity;
+
+    public Product (String initialName, double initialPrice, int initialQuantity) {
+        this.Name = initialName;
+        this.Price = initialPrice;
+        this.Quantity = initialQuantity;
 
     }
 
     public void printProduct() {
-        System.out.println();
+        System.out.println(this.Name + ", price " + this.Price + ", " + this.Quantity + " pcs");
     }
 }
