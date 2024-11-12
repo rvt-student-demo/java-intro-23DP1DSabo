c610ee9f8b87a66e287343fea6c1d4f0f7a2e07e
23DP1DSabo <166118166+23DP1DSabo@users.noreply.github.com>
2024-11-12 08:36:10 +0000

    Printing a File

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,12 +6,10 @@ import java.util.*;
 public class App {
     public static void main( String[] args ) throws Exception {
 
-            BufferedReader reader = Utils.getReader("data.csv");
+            BufferedReader reader = Utils.getReader("printingAFile.csv");
             String line;
-
-            while ((line = reader.readLine()) != null) {
+                while ((line = reader.readLine()) != null)
                 System.out.println(line);
             }
 
-    }
 }
